9d7d4f2147a6d2db9f35ae3f5ef2e40384531c6b
Picoche <hombert.fabien@gmail.com>
2023-11-23 12:24:27 +0100

    mise en place connexion fictive

--- app/src/main/java/com/panierdantan/auth/repositories/AuthRepository.kt
+++ app/src/main/java/com/panierdantan/auth/repositories/AuthRepository.kt
@@ -1,12 +1,17 @@
 package com.panierdantan.auth.repositories
 
-import com.panierdantan.R
+import io.realm.kotlin.mongodb.User as RealmUser
+import com.panierdantan.atlas_collections.accounts.User
+import com.panierdantan.atlas_collections.produits.Produit
+import com.panierdantan.atlas_collections.shops.Boutique
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.realm.kotlin.Realm
 import io.realm.kotlin.mongodb.App
-import io.realm.kotlin.mongodb.AppConfiguration
+import io.realm.kotlin.mongodb.Credentials
+import io.realm.kotlin.mongodb.sync.SyncConfiguration
 import javax.inject.Singleton
 
 class AuthRepository {
@@ -16,10 +21,29 @@ class AuthRepository {
 
         @Singleton
         @Provides
-        fun provideRealmAppConfig() {
-            val app = App.create("application-0-vlpzl")
+        fun createRealmAppConfig(): App {
+            return App.create("application-0-vlpzl")
+        }
 
+        @Singleton
+        @Provides
+        suspend fun LogInUser(app: App): RealmUser {
+            val credentials = Credentials.emailPassword("hombert.fabien@gmail.com", "@Renouvier66")
+            return app.login(credentials)
+        }
 
+        @Singleton
+        @Provides
+        fun provideRealmAppConfig(user: RealmUser): Realm {
+            val config =
+                SyncConfiguration.Builder(user, setOf(User::class, Produit::class, Boutique::class))
+                    .build()
+
+            return Realm.open(config)
+        }
+
+        fun closeThisRealm(realm: Realm) {
+            realm.close()
         }
     }
 }
--- app/src/main/java/com/panierdantan/auth/view_models/UserViewModel.kt
+++ app/src/main/java/com/panierdantan/auth/view_models/UserViewModel.kt
@@ -1,23 +1,40 @@
 package com.panierdantan.auth.view_models
 
 import androidx.lifecycle.ViewModel
-import com.panierdantan.R
+import androidx.lifecycle.viewModelScope
+import com.panierdantan.atlas_collections.shops.Boutique
 import com.panierdantan.auth.repositories.AuthRepository
+import com.panierdantan.auth.repositories.AuthRepository.AppModule
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.realm.kotlin.Realm
+import io.realm.kotlin.ext.query
 import io.realm.kotlin.mongodb.App
-import io.realm.kotlin.mongodb.Credentials
-import io.realm.kotlin.mongodb.AppConfiguration
 import io.realm.kotlin.mongodb.User
+import io.realm.kotlin.query.RealmResults
 import com.panierdantan.atlas_collections.accounts.User as Utilisateur
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor(private val repo: AuthRepository) : ViewModel() {
 
-    val credentials = Credentials.emailPassword("hombert.fabien@gmail.com", "@Renouvier66")
+    val app = AppModule.createRealmAppConfig()
 
+    val userId = MutableStateFlow<Int>(0)
     val user = MutableStateFlow<Utilisateur>(Utilisateur())
+
+    fun logInUser(app: App) {
+        viewModelScope.launch {
+            val userToLogIn = AppModule.LogInUser(app)
+            userId.value = userToLogIn.id.toInt()
+            val realm = AppModule.provideRealmAppConfig(userToLogIn)
+
+            user.value = realm.write {
+                realm.query<Utilisateur>(Utilisateur::class).equalTo("id", userId.value).findFirst()!!
+            }
+        }
+    }
 
     // val user = app.login(credentials)
 }
